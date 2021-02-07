1d655fddbc1a9be5e075b8ca5c534cbf0f04e19c
lifanchao <lifc@yisa.com>
2021-02-07 12:00:12 +0800

    提升kotlin版本

--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.kotlindemo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
+    val TAG = "MainActivity"
+    var count = 42
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +29,24 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent);
 
         }
+
+
+        //toUpperCase() 是一个只能对 String 类型的变量调用的函数
+        val langName = "Kotlin"
+        val upperCaseName = langName.toUpperCase();
+        Log.e(TAG, "onCreate:====> " + upperCaseName)
+
+        //要使变量持有 null 值，它必须是可为 null 类型。可以在变量类型后面加上 ? 后缀，将变量指定为可为 null，如以下示例所示：
+        val  langNull : String? = null
+        Log.e(TAG, "onCreate: 2......>>>" + langNull )
+
+        //随着条件语句的复杂性不断增加，您可以考虑将 if-else 表达式替换为 when 表达式，如以下示例所示：
+        val answerString = when {
+            count == 42 -> "I have the answer."
+            count > 35 -> "The answer is close."
+            else -> "The answer eludes me."
+        }
+        Log.e(TAG, "onCreate: ===3===>" + answerString )
+
     }
 }
